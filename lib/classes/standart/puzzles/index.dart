9e3cfd9493260278ce2558e95fec185e7c1b394d
Marvin Quevedo <marvinquevedo2017@outlook.es>
2023-04-03 18:49:11 -0600

    reorder wallet sdk code

--- lib/classes/standart/puzzles/index.dart
+++
@@ -1,3 +0,0 @@
-export 'calculate_synthetic_public_key/calculate_synthetic_public_key.clvm.hex.dart';
-export 'p2_delegated_puzzle_or_hidden_puzzle/p2_delegated_puzzle_or_hidden_puzzle.clvm.hex.dart';
-export 'p2_conditions/p2_conditions.dart';
--- lib/classes/standart/standart.dart
+++
@@ -1,2 +0,0 @@
-export 'puzzles/index.dart';
-export 'utils.dart';
