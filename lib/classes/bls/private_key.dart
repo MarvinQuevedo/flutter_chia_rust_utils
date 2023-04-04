2188690e3e4d1c927783ca74d4800089fa905aee
Marvin Quevedo <marvinquevedo2017@outlook.es>
2023-04-03 18:47:05 -0600

    add from hex, bytes and stream for compatibility

--- lib/classes/bls/private_key.dart
+++ lib/classes/bls/private_key.dart
@@ -1,12 +1,25 @@
 import 'dart:typed_data';
- 
 
 import '../../ffi.io.dart';
 import '../bytes_utils/bytes.dart';
 import 'public_key.dart';
 
 class PrivateKey extends Bytes {
+  static const int size = 32;
   PrivateKey(Uint8List bytes) : super(bytes);
+
+  factory PrivateKey.fromBytes(Uint8List bytes) {
+    return PrivateKey(bytes);
+  }
+
+  factory PrivateKey.fromHex(String bytesHex) {
+    return PrivateKey(Bytes.fromHex(bytesHex).byteList);
+  }
+
+  factory PrivateKey.fromStream(Iterator<int> iterator) {
+    final bytes = iterator.extractBytesAndAdvance(size);
+    return PrivateKey.fromBytes(bytes.byteList);
+  }
 
   Future<PrivateKey> derivePathHardened(List<int> path) async {
     final Uint32List pathBytes = Uint32List.fromList(path);
@@ -30,6 +43,10 @@ class PrivateKey extends Bytes {
   Future<PublicKey> publicKey() async {
     final result = await api.secretKeyPublicKey(sk: byteList);
     return PublicKey(result);
+  }
+
+  Future<PublicKey> getG1() {
+    return publicKey();
   }
 
   /// Private key from seed
