ada4ba26dff42034fccc1b82eefcf96e68f82628
Marvin Quevedo <marvinquevedo2017@outlook.es>
2023-08-27 21:41:59 -0600

    try to build web

--- scripts/build-web.sh
+++ scripts/build-web.sh
@@ -3,4 +3,5 @@ rustup +nightly component add rust-src
 rustup +nightly target add wasm32-unknown-unknown
 cargo install wasm-pack
 
-cargo build -r --target=wasm32-unknown-unknown
+#cargo build -r --target=wasm32-unknown-unknown
+wasm-pack build --target web
