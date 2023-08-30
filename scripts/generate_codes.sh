7cd940451f6f9feb714710e42c7a23a57afcaa03
Marvin Quevedo <marvinquevedo2017@outlook.es>
2023-08-29 20:59:29 -0600

    try to genere code gen

--- scripts/generate_codes.sh
+++ scripts/generate_codes.sh
@@ -1,13 +1,13 @@
  
 
-flutter_rust_bridge_codegen --wasm --rust-input chia_rust_utils/src/api.rs \
-                            --dart-output lib/generated/bridge_generated.dart 
-
+# flutter_rust_bridge_codegen --wasm --rust-input chia_rust_utils/src/api.rs \
+#                             --dart-output lib/generated/bridge_generated.dart 
+cargo install flutter_rust_bridge_codegen  
+ 
 flutter_rust_bridge_codegen \
-    -r chia_rust_utils/src/api.rs  \
+    --wasm \
+    --rust-input chia_rust_utils/src/api.rs  \
     --llvm-path /opt/homebrew/opt/llvm/ \
-    -d lib/generated/bridge_generated.dart \
-    -c ios/Classes/BridgeGenerated.h \
-    -e macos/Classes/   # if building for MacOS, extra path is essential
+    --dart-output lib/generated/bridge_generated.dart  -c ios/Classes/BridgeGenerated.h   -e macos/Classes/    
  
  cp ios/Classes/BridgeGenerated.h  macos/Classes/
